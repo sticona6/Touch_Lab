bf1dac7abafa6d69e4cad25e0947dda639d2cae4
TJCHcrcm <hxl3131@gmail.com>
2017-10-14 22:42:37 -0500

    iserte un int

--- app/src/main/java/com/upt/touch/MainActivity.java
+++ app/src/main/java/com/upt/touch/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int hola = 10;
-
+sdf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
